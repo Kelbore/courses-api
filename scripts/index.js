7cc6c90711fc72556aa21c26375f053e4ce965b9
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-06-07 16:03:21 -0400

    courses api

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const tableDiv = document.getElementById('courseTable');
+const tbody = document.querySelector('#courseTable tbody');
+
+const coursesAPI = fetch('http://localhost:8081/api/courses');
+coursesAPI.then((response) => response.json()).then((data) => {
+    data.forEach(element => {
+       const row = tbody.insertRow(-1);
+       
+       const cell1 = row.insertCell(0);
+       cell1.innerHTML = element.dept;
+       const cell2 = row.insertCell(1);
+       cell2.innerHTML = element.courseNum;
+       const cell3 = row.insertCell(2);
+       cell3.innerHTML = element.courseName;
+       const cell4 = row.insertCell(3);
+       cell4.innerHTML = `See details`;
+    });
+});
